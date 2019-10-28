22be783fc6e10b42f4c60cfa7b3d7100fbcd1deb
DankoMaksym <dankomaksym@gmail.com>
2019-10-28 13:34:39 +0200

    counter of clicks function with localStorage

--- public/script.js
+++ public/script.js
@@ -1,8 +1,6 @@
-"use strict"; 
-
-const h = () => {
-
-	let x = window.localStorage.getItem('aaa');
-	window.localStorage.setItem('aaa', 555);
-	alert(x);
+const hello = () => {
+	var x = localStorage.getItem('key');
+	window.alert(`Hello (greeted ${x} people)`);
+	x = x * 1 + 1;
+	localStorage.setItem('key', x);
 }
