059ce9b68f8ef8e4e952711bb703edfef3f19f98
DankoMaksym <dankomaksym@gmail.com>
2019-10-28 16:27:31 +0200

    seed products with 3 pizzas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_120850) do
+ActiveRecord::Schema.define(version: 2019_10_28_124015) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
