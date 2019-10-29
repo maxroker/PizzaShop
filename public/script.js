61f017905f1c4a4c7bb4812fbb12b92386d25ae4
DankoMaksym <dankomaksym@gmail.com>
2019-10-29 13:10:34 +0200

    add to cart functionality

--- public/script.js
+++ public/script.js
@@ -4,3 +4,12 @@ const hello = () => {
 	window.alert(`Hello (greeted ${x} people)`);
 	localStorage.setItem('key', x);
 }
+
+const addToCart = (pizza, id) => {
+	let pizzaKey = 'pizza_' + id;
+	let pizzaItem = window.localStorage.getItem(pizzaKey) * 1;
+	pizzaItem += 1;
+	window.localStorage.setItem(pizzaKey, pizzaItem);
+
+	alert(`You added ${pizza} in a cart (${id} id of chosen pizza)`);
+}
